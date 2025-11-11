a14be9314e3cdadd77e511507d548a463481098b
xDennis20 <dennis_6@outlook.com>
2025-11-10 19:43:22 -0500

    Mejorando el codigo del problema 16 haciendolo mas facil de entender

--- Semana-2/Problema16.py
+++ Semana-2/Problema16.py
@@ -8,15 +8,15 @@ def ordenar_2listas(lista1: list[int],lista2: list[int]) -> list[int]:
     resultado = []
     i = 0
     j = 0
-    if not lista1 and lista2:
-        return resultado
+
     while i < len(lista1) and j < len(lista2):
         if lista1[i] <= lista2[j]:
             resultado.append(lista1[i])
             i+=1
-        elif lista1[i] >= lista2[j]:
+        else:
             resultado.append(lista2[j])
             j+=1
+
     resultado.extend(lista1[i:])
     resultado.extend(lista2[j:])
     return resultado
