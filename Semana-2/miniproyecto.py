db20aff30738f1d9e8926e8d242012eeced2b724
xDennis20 <dennis_6@outlook.com>
2025-11-11 19:46:35 -0500

    Agregue nuevas funciones que usan la persistencia con el archivo JSON, para tener los archivos siempre guardados

--- Semana-2/miniproyecto.py
+++ Semana-2/miniproyecto.py
@@ -1,3 +1,4 @@
+import json
 class Tarea:
     def __init__(self,id: int,descripcion: str,estado: bool):
         self.descripcion = descripcion
@@ -11,8 +12,8 @@ class GestorTarea:
 
     def agregar_tarea(self):
         descripcion_tarea = input("Ingrese la descripcion de su tarea: ")
-        id = len(self.tareas) + 1
-        self.tareas.append(Tarea(id,descripcion_tarea,False))
+        ids = len(self.tareas) + 1
+        self.tareas.append(Tarea(ids,descripcion_tarea,False))
 
     def completar_tarea(self):
         buscar_id = int(input("Ingrese el id de la tarea que desea completar: "))
@@ -30,6 +31,19 @@ class GestorTarea:
         for tarea in self.tareas:
             print(f"{"[x]" if tarea.estado is True else "[]"} {tarea.id}: {tarea.descripcion}")
 
+    def cargar_tarea(self,archivo = "tarea.json"):
+        tarea = [{"id": t.id,"descripcion": t.descripcion,"estado": t.estado} for t in self.tareas]
+        with open(archivo,"w") as f:
+            json.dump(tarea,f)
+        print("Tareas guardado con exito")
+
+    def cargar_archivo(self,archivo = "tarea.json"):
+        try:
+            with open(archivo,"r") as f:
+                tareas = json.load(f)
+                self.tareas = [Tarea(t["id"],t["descripcion"],t["estado"]) for t in tareas]
+        except FileNotFoundError:
+            self.tareas = []
 
 to_do = GestorTarea()
 to_do.agregar_tarea()
@@ -37,3 +51,4 @@ to_do.agregar_tarea()
 to_do.agregar_tarea()
 to_do.eliminar_tarea()
 to_do.mostrar_tarea()
+to_do.cargar_tarea()
