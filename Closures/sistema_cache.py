56c371f22945b8ee3a2ff872cadfc71102d43b20
xDennis20 <dennis_6@outlook.com>
2026-01-28 08:40:52 -0500

    add: eliminar cache menos usado cuando tenga mas de 100 almacenados

--- Closures/sistema_cache.py
+++ Closures/sistema_cache.py
@@ -1,8 +1,10 @@
 """PROYECTO: Sistema de Caché Manual (60-90 min)
 Objetivo:
 Crear una función que "cachee" (guarde) resultados de operaciones costosas para evitar recalcularlas."""
-def crear_cache():
-    funciones_cache = {}
+from collections import OrderedDict
+
+def crear_cache(max_size=100):
+    funciones_cache = OrderedDict()
     misses = 0
     hit = 0
     def ejecutar_con_cache(funcion,*args):
@@ -10,9 +12,12 @@ def crear_cache():
         clave_cache = funcion.__name__, args
         if clave_cache in funciones_cache:
             hit += 1
+            funciones_cache.move_to_end(clave_cache)
             return funciones_cache.get(clave_cache)
         else:
             resultado_funcion = funcion(*args)
+            if len(funciones_cache) >= max_size:
+                funciones_cache.popitem(last=False)
             funciones_cache[clave_cache] = resultado_funcion
             misses += 1
             return resultado_funcion
