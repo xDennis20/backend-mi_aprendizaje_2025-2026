a1e56e808ffae64cb5f32faabe6ebf28253f9335
xDennis20 <dennis_6@outlook.com>
2026-02-03 15:03:31 -0500

    add: Nuevo ejercicio de decoradores

---
+++ Decoradores/ejercicio_11.py
@@ -0,0 +1,21 @@
+def cache_simple(func):
+    cache = {}
+    def wrapper():
+        if func not in cache:
+            resultado = func()
+            cache[func] = resultado
+        return cache.get(func)
+    return wrapper
+
+@cache_simple
+def obtener_configuracion():
+    print("Consultando BD...")
+    return {"tema": "oscuro", "idioma": "es"}
+
+# Primera llamada
+config1 = obtener_configuracion()  # "Consultando BD..."
+
+# Segunda llamada
+config2 = obtener_configuracion()  # NO imprime (usa caché)
+
+print(config1 is config2)  # True (mismo objeto)
