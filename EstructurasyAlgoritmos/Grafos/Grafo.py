76b6bcc832f2f084a59b9aa29a6b3a57e1ce2b5f
xDennis20 <dennis_6@outlook.com>
2026-03-10 16:23:26 -0500

    new: Funcion buscar con el algoritmo dfs

--- EstructurasyAlgoritmos/Grafos/Grafo.py
+++ EstructurasyAlgoritmos/Grafos/Grafo.py
@@ -13,3 +13,30 @@ class Grafo:
             self.nodos.get(origen).agregar_vecino(destino,peso)
         else:
             print("Nodo no encontrado en el grafo")
+
+    def buscar_dfs(self,inicio: str,  valor: str, visitados: set = None) -> bool:
+        if len(valor.strip()) == 0:
+            return False
+
+        if visitados is None:
+            visitados = set()
+
+        visitados.add(inicio)
+        print(f"Visitando {inicio}")
+
+        if inicio == valor:
+            print(f"Encontrado {inicio}")
+            return True
+
+        nodo_actual = self.nodos.get(inicio)
+        if nodo_actual is None:
+            return False
+
+        for vecino in nodo_actual.vecinos:
+            if vecino not in visitados:
+                if self.buscar_dfs(vecino, valor, visitados):
+                    return True
+        return False
+
+    def __str__(self):
+        return f"Grafo: {self.nodos}"
