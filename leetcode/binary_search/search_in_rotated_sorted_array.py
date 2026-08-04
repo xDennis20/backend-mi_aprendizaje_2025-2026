77cb8dc93e2a4befc56d7b773c04f8e7c8f108e2
xDennis20 <dennis_6@outlook.com>
2026-08-04 12:09:34 -0500

    feat: ejercicio de leetcode binary_search: search in rotated sorted array

---
+++ leetcode/binary_search/search_in_rotated_sorted_array.py
@@ -0,0 +1,23 @@
+def search(nums: list[int], target: int) -> int:
+    right = len(nums) - 1
+    left = 0
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return -1
+
+print(search([0,1,2,4,5,6,7],2))
+print(search([4,5,6,7,0,1,2],2))
