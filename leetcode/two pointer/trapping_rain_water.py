172066c5417f8f6827985b4301390825479e4a2d
xDennis20 <dennis_6@outlook.com>
2026-08-20 10:40:12 -0500

    feat: resolvi de nuevo el ejercicio de leetcode two pointers: trapping rain water

--- leetcode/two pointer/trapping_rain_water.py
+++ leetcode/two pointer/trapping_rain_water.py
@@ -1,24 +1,26 @@
 def trap(height: list[int]) -> int:
     #inicializar las variables: (max_left, max_right, trap_water)
-    trap_water = 0
     left = 0
     right = len(height) - 1
-    max_num_left = height[left]
-    max_num_right = height[right]
+    max_l = height[left]
+    max_r = height[right]
+    trap_water = 0
     #recorrer la lista dada mediante 2 punteros izquierda y derecha, y inicializando los max_num con el valor del indice.
     while left < right:
         #Si el max_num_right es mayor que el de la izquierda, aumentamos a left + 1, y agarramos el maximo entre el anterior max_num y el nuevo numero de la lista con el indice de left,
         # y calculamos el max con el numero del indice actual,
-        if max_num_left < max_num_right:
-            left += 1
-            max_num_left = max(max_num_left,height[left])
-            trap_water += max(0,max_num_left - height[left])
-        else:
+        if max_l <= max_r:
+            left+=1
+            altura_actual = height[left]
+            max_l = max(max_l, altura_actual)
+            trap_water += max(0, max_l - altura_actual)
             # Si el max_num_left es mayor que el de la derecha, restamos right - 1, y agarramos el maximo entre el anterior max_num y el nuevo numero de la lista con el indice de right,
             # y calculamos el max con el numero del indice actual,
-            right -= 1
-            max_num_right = max(max_num_right, height[right])
-            trap_water += max(0,max_num_right - height[right])
+        else:
+            right-=1
+            altura_actual = height[right]
+            max_r = max(max_r, altura_actual)
+            trap_water += max(0, max_r - altura_actual)
 
     return trap_water
 
@@ -41,7 +43,5 @@ def trap_1(height: list[int]) -> int:
 
     return trap_water
 
-print(trap_1([4,2,0,3,2,5]))
-print(trap_1([0,1,0,2,1,0,1,3,2,1,2,1]))
-# print(trap([4,2,0,3,2,5]))
-# print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+print(trap([4,2,0,3,2,5]))
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
