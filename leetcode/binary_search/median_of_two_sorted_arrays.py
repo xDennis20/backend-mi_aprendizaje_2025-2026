b7778a2b54d96af46f557d6be673297388f81944
xDennis20 <dennis_6@outlook.com>
2026-08-26 15:25:14 -0500

    feat: ejercicio de leetcode binary_search: median of two sorted arrays

---
+++ leetcode/binary_search/median_of_two_sorted_arrays.py
@@ -0,0 +1,31 @@
+def find_median_sorted_arrays( nums1: list[int], nums2: list[int]) -> float:
+    if len(nums1) > len(nums2):
+        return find_median_sorted_arrays(nums2,nums1)
+    total = len(nums1) + len(nums2)
+    if total % 2 == 0:
+        particion = total // 2
+    else:
+        particion = (total + 1) // 2
+    left = 0
+    right = len(nums1)
+
+    while left <= right:
+        mid_a = (left + right) // 2
+        mid_b = (particion - mid_a)
+
+        a_izq = nums1[mid_a - 1] if mid_a > 0 else float("-inf")
+        a_der = nums1[mid_a] if mid_a < len(nums1) else float("inf")
+
+        b_izq = nums2[mid_b - 1] if mid_b > 0 else float("-inf")
+        b_der = nums2[mid_b] if mid_b < len(nums2) else float("inf")
+
+        if not a_izq <= b_der:
+            right = mid_a - 1
+        elif not b_izq <= a_der:
+            left = mid_a + 1
+        else:
+            if total % 2 == 0:
+                return (max(a_izq,b_izq) + min(a_der,b_der)) / 2
+            return max(a_izq,b_izq)
+
+print(find_median_sorted_arrays([1,3],[2]))
