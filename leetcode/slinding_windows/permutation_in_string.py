6f9384605a2f555e6453eec95bc1ba9f185e7e9e
xDennis20 <dennis_6@outlook.com>
2026-09-01 09:36:52 -0500

    feat: ejercicio de leetcode slinding windows: permutation in string repiticion de nuevo

--- leetcode/slinding_windows/permutation_in_string.py
+++ leetcode/slinding_windows/permutation_in_string.py
@@ -1,28 +1,28 @@
 def check_inclusion(s1: str, s2: str) -> bool:
-    letras_s1: dict[str,int] = {}
-    for s in s1:
-        if s not in letras_s1:
-            letras_s1[s] = 1
+    ch_freq: dict[str,int] = {}
+    for ch in s1:
+        if ch not in ch_freq:
+            ch_freq[ch] = 1
         else:
-            letras_s1[s] += 1
+            ch_freq[ch] += 1
 
     left = 0
 
-    for right, ch in enumerate(s2):
-        if ch in letras_s1:
-            letras_s1[ch] -= 1
-            while letras_s1[ch] < 0:
-                if s2[left] in letras_s1:
-                    letras_s1[s2[left]] += 1
-                left += 1
-            if (right - left) + 1 == len(s1):
+    for i, ch in enumerate(s2):
+        if ch in ch_freq:
+            ch_freq[ch] -= 1
+            while ch_freq[ch] < 0:
+                if s2[left] in ch_freq:
+                    ch_freq[s2[left]] += 1
+                left+=1
+            if (i - left) + 1 == len(s1):
                 return True
         else:
-            while left < right:
-                if s2[left] in letras_s1:
-                    letras_s1[s2[left]] += 1
+            while left < i:
+                if s2[left] in ch_freq:
+                    ch_freq[s2[left]] += 1
                 left += 1
-            left = right + 1
+            left = i + 1
 
     return False
 
