20f82abd93975915178cb37bde365ab9b54821f7
xDennis20 <dennis_6@outlook.com>
2026-09-14 09:32:27 -0500

    feat: repeticion ejercicio de leetcode binary search: koko eating bananas

--- leetcode/binary_search/koko_eating_bananas.py
+++ leetcode/binary_search/koko_eating_bananas.py
@@ -4,21 +4,21 @@ def min_eating_speed(piles: list[int], h: int) -> int:
     v_left = 1
     v_right = max(piles)
     total_horas = 0
-    res = 0
+    recuerdo = 0
 
     while v_left <= v_right:
-        v_mid = (v_right + v_left) // 2
-        for pila in piles:
-            total_horas += ceil(pila / v_mid)
+        mid = (v_right + v_left) // 2
+        for pile in piles:
+            total_horas += ceil(pile / mid)
         if total_horas <= h:
-            res = v_mid
-            v_right = v_mid - 1
-            total_horas = 0
+            v_right = mid - 1
+            recuerdo = mid
         else:
-            v_left = v_mid + 1
-            total_horas = 0
+            v_left = mid + 1
+        total_horas = 0
 
-    return res
+    return recuerdo
 
 
 print(min_eating_speed([25,10,23,4], 4))
+print(ceil(3/4))
