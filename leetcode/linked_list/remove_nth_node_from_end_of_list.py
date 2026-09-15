0c7b19425faa8c93daae535a030f9537fa91c598
xDennis20 <dennis_6@outlook.com>
2026-09-15 09:12:00 -0500

    feat: ejercicio de leetcode linked list: remove nth node from end of list

---
+++ leetcode/linked_list/remove_nth_node_from_end_of_list.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+def remove_nth_from_end(head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    dummy = ListNode(0, head)
+    slow = dummy
+    fast = dummy
+
+    for _ in range(n):
+        fast = fast.next
+
+    while fast.next:
+        slow = slow.next
+        fast = fast.next
+
+    slow.next = slow.next.next
+    return dummy.next
