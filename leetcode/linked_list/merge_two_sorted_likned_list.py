6bff1a43f0a92c03e61186733581534d4627d305
xDennis20 <dennis_6@outlook.com>
2026-09-21 10:24:58 -0500

    feat: ejercicio de leetcode linked list: merge two sorted linked list

--- leetcode/linked_list/merge_two_sorted_likned_list.py
+++ leetcode/linked_list/merge_two_sorted_likned_list.py
@@ -10,17 +10,22 @@ def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Opt
     pointer = dummy
 
     while list1 and list2:
+        recuerdo_siguiente_1 = list1.next
+        recuerdo_siguiente_2 = list2.next
+
         if list1.val <= list2.val:
             pointer.next = list1
-            pointer = pointer.next
-            list1 = list1.next
+            list1 = recuerdo_siguiente_1
         else:
             pointer.next = list2
-            pointer = pointer.next
-            list2 = list2.next
+            list2 = recuerdo_siguiente_2
 
-    pointer.next = list1 or list2
+        pointer = pointer.next
+
+    if list1:
+        pointer.next = list1
+
+    elif list2:
+        pointer.next = list2
 
     return dummy.next
-
-
