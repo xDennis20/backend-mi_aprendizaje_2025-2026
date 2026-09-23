f698ece19e247c7d60f89bf59c1ca873c2060cd5
xDennis20 <dennis_6@outlook.com>
2026-09-23 09:44:50 -0500

    feat: ejercicio de leetcode slinding windows: sliding window maximum

--- leetcode/slinding_windows/sliding_window_maximum.py
+++ leetcode/slinding_windows/sliding_window_maximum.py
@@ -3,7 +3,7 @@ from collections import deque
 def max_sliding_window( nums: list[int], k: int) -> list[int]:
     left = 0
     cola = deque()
-    resultados = []
+    resultado = []
 
     for right, num in enumerate(nums):
         while cola and nums[cola[-1]] <= num:
@@ -15,10 +15,10 @@ def max_sliding_window( nums: list[int], k: int) -> list[int]:
             cola.popleft()
 
         if (right - left) + 1 == k:
-            resultados.append(nums[cola[0]])
-            left+=1
+            resultado.append(nums[cola[0]])
+            left += 1
 
-    return resultados
+    return resultado
 
 print(max_sliding_window([1,3,-1,-3,5,3,6,7], 3))
 print(max_sliding_window([1,2,1,0,4,2,6], 3))
