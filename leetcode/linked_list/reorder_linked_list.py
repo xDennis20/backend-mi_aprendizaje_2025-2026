000cead60976963a4e3bbcdc9b9a87024fb9da5a
xDennis20 <dennis_6@outlook.com>
2026-09-24 10:18:18 -0500

    feat: repeticion del ejercicio de linked list: reorder linked list

--- leetcode/linked_list/reorder_linked_list.py
+++ leetcode/linked_list/reorder_linked_list.py
@@ -6,7 +6,6 @@ class ListNode:
          self.next = next
 
 def reorder_list(head: Optional[ListNode]) -> None:
-    """Sacar la mitad de la lista enlazada"""
     slow = head
     fast = head
 
@@ -17,22 +16,23 @@ def reorder_list(head: Optional[ListNode]) -> None:
     segunda_mitad = slow.next
     slow.next = None
 
-    """Invertir la mitad de la lista sacada"""
+    """Invertir la lista"""
     recuerdo_anterior = None
-
     while segunda_mitad:
         recuerdo_siguiente = segunda_mitad.next
         segunda_mitad.next = recuerdo_anterior
         recuerdo_anterior = segunda_mitad
         segunda_mitad = recuerdo_siguiente
 
-    """Unir las 2 listas segun el orden"""
+    """Unir las 2 listas"""
     segunda_mitad = recuerdo_anterior
     primera_mitad = head
     while segunda_mitad:
-        siguiente1 = primera_mitad.next
-        siguiente2 = segunda_mitad.next
+        recuerdo_siguiente_1 = primera_mitad.next
+        recuerdo_siguiente_2 = segunda_mitad.next
+
         primera_mitad.next = segunda_mitad
-        segunda_mitad.next = siguiente1
-        primera_mitad = siguiente1
-        segunda_mitad = siguiente2
+        segunda_mitad.next = recuerdo_siguiente_1
+
+        primera_mitad = recuerdo_siguiente_1
+        segunda_mitad = recuerdo_siguiente_2
